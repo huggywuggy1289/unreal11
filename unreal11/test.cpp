9dcff0a01307a8550a366fc8f006313f4c1ae274
huggywuggy1289 <bangstation0714@naver.com>
2026-07-13 12:07:15 +0900

    입력받기 출력하기

---
+++ unreal11/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;// = std::cout
+
+int main() {
+
+	// 출력 : cout로 "hello world!" 출력하기
+	int age;
+	string name;
+	cout << "hello world!\n";
+
+	//입력: cin으로 나이를 입력받아
+	cout << "나이를 입력하세요: ";
+	cin >> age;
+	cout << "이름도 입력하세요: ";
+	cin >> name;
+	// 출력 : 입력받은 나이를 출력하기
+	cout << "(만)" << age << "세 " << name << "님 안녕하세요~ 프로그래밍하기 딱 좋은 나이네요" << endl;
+
+	return 0;
+}
