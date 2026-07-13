969b4336f02dd0afca6daaeefb6dcb98fc823b29
huggywuggy1289 <bangstation0714@naver.com>
2026-07-13 12:35:01 +0900

    버그찾기

---
+++ unreal11/realTest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+// 1 int, char, float, bool
+
+// 2
+int main() {
+	int score;
+	const int MaxScore = 100;
+
+	score + 10; // (1) score값이 초기화 되지않아서 쓰레기값 가지고 있음. 또한 이꼴표시가 없어서 값이 어디서도 저장이 안됨.
+	MaxScore = 200; // (2) const로 선언된 변수는 값 변경 불가
+	char myGrade = "A"; //(3) char형 변수는 작은 따옴표('')로 감싸야함
+
+	return 0;
+}
--- unreal11/test1.cpp
+++ unreal11/test1.cpp
@@ -12,7 +12,7 @@ int main()
 	float height = 180.5f;
 	double PI = 3.141592;
 	char grade = 'A';
-	bool isAlive = true;
+	bool isAlive = true; // stack에 isAlive라는 이름으로 bool형 변수가 생기고 true(내부적으로는 1)이 저장됨.
 
 	cout << "나이: " << age << endl;
 	cout << "체력: " << hp << endl;
@@ -21,4 +21,6 @@ int main()
 	cout << "원주율: " << PI << endl;
 	cout << "학점: " << grade << endl;
 	cout << "살아있음: " << isAlive << endl;
+	isAlive = false; // 값이 덮어씌워짐. 기존 공간 재사용 (덮어쓰기)
+	cout << "isAlive? : " << (isAlive ? "Yeah~" : "No...") << endl;
 }
