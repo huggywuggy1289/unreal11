5cdca6222262a7fea260628c7dc9220071b8383c
huggywuggy1289 <bangstation0714@naver.com>
2026-07-14 22:50:53 +0900

    점수 등급 출력

--- unreal11/week2_work1.cpp
+++ unreal11/week2_work1.cpp
@@ -1,6 +1,10 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) cout << "짝수" << endl;
+    else cout << "홀수" << endl;
+    return 0;
+}
---
+++ unreal11/week2_work10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/*
+int main() {
+    int answer = 50;
+    int guess;
+
+    while (true) {
+        cin >> guess;
+
+        if (guess > answer) {
+            cout << "DOWN" << endl;
+        }
+        else if (guess < answer) {
+            cout << "UP" << endl;
+        }
+        else {
+            cout << "SUCCESS" << endl;
+            break;   // 정답 맞히면 반복 탈출
+        }
+    }
+
+    return 0;
+}
+*/
