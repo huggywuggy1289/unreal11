7592e3e83d63d0a72b3b5fdbd33edb85836d1a2f
huggywuggy1289 <bangstation0714@naver.com>
2026-07-14 20:14:57 +0900

    섭씨 화씨 변환

--- unreal11/week1_work2.cpp
+++ unreal11/week1_work2.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+/*
 int main() {
 	float pie = 3.14;
 	int r;
@@ -10,9 +11,10 @@ int main() {
 	cout << "반지름을 입력하세요" << endl;
 	cin >> r;
 
-	int width = r * r * pie;
+	int width = r * r * pie; // 입력받기 전에 미리 선언하면 쓰레기값이 들어있다.
 
 	cout << "원의 넓이는 " << width << "입니다." << endl;
 
 	return 0;
 }
+*/
---
+++ unreal11/week1_work3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double c, f; // 섭씨, 화씨
+
+	cin >> c;
+
+	f = (c * 9 / 5) + 32;
+
+	cout << f << endl;
+
+	return 0;
+
+
+}
